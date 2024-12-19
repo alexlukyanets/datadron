09dc3eb7f89398c436e9d20ced9ae193660ba6ac
Oleksandr <olukianets@lytvynov-production.com>
2024-12-19 20:50:49 +0100

    update template

--- src/core/client/client.py
+++ src/core/client/client.py
@@ -1,9 +1,25 @@
+from asyncio import Semaphore
+from collections import defaultdict
+
 from httpx import AsyncClient
+
+from core.client import consts
 
 
 class DataDroneClient:
-    def __init__(self):
-        self.client: AsyncClient = AsyncClient()
+    client: AsyncClient = AsyncClient()
+
+    def __init__(
+        self,
+        async_tasks: int = consts.ASYNC_TASKS,
+        retry_times: int = consts.RETRY_TIMES,
+        headers: dict[str, str] | None = None
+    ):
+        self.semaphore: Semaphore = Semaphore(value=async_tasks)
+        self.retry_times: int = retry_times
+        self.status_counter: dict[int, int] = defaultdict(int)
+        self.headers: dict[str, str] | None = headers
+        self.random_user_agent: bool = False
 
     async def request(self, method: str, url: str, **kwargs):
         return await self.client.request(method, url, **kwargs)
---
+++ src/core/client/consts.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+ASYNC_TASKS: Final[int] = 10
+
+RETRY_TIMES: Final[int] = 10
